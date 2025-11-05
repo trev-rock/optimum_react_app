5eef59a00122a8b011ae2a1962dce4100a4d64b6
Trevor Bernard <tbernard260@gmail.com>
2025-11-05 01:56:47 -0500

    night update 3
    added storing the favorited movies to local storage

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,9 +2,26 @@ import { useState, useEffect } from "react"
 
 export default function Movie({ movie, viewMovie }) {
     const [favorite, setFavorite] = useState(false)
-    
+
+    useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || []
+    if (storedFavorites.includes(movie.id)) {
+        setFavorite(true)
+    }
+    }, [movie.id])
+
     function toggleFavorite(){
-        setFavorite(prev => !prev)
+        setFavorite(prev => {
+        const favoriteItem = !prev 
+        const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || []
+        if (favoriteItem) {
+            localStorage.setItem("favorites", JSON.stringify([...storedFavorites, movie.id]))
+        } else {
+            const update = storedFavorites.filter(id => id != movie.id)
+            localStorage.setItem("favorites", JSON.stringify(update))
+        }
+        return favoriteItem
+        })
     }
     return (
     <>
